f913beab33ebe397ba46bac860adc48da4a94ed4
shaghayegh0 <shaghayegh.dehghanis@ryerson.ca>
2025-06-07 22:47:31 -0400

    Initial Commit

---
+++ MealLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealLens
+//
+//  Created by sherry sanij on 2025-06-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealLens/MealLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealLensApp.swift
+//  MealLens
+//
+//  Created by sherry sanij on 2025-06-07.
+//
+
+import SwiftUI
+
+@main
+struct MealLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
